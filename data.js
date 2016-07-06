3deb595a3bf1f795eabaa3397a30cadadbade5fc
Yasiru Kassapa <wik2kassa@gmail.com>
2016-07-07 03:39:44 +0530

    Changes

---
+++ data.js
@@ -0,0 +1,22 @@
+/**
+ * Created by wik2kassa on 7/7/16.
+ */
+
+var GPW = [
+    {
+        id : 0,
+        speakerName : "Nitin Bawankule",
+        designation : "Director Google India",
+        speechTitle : "The Business Case for Creating a Great Workplace",
+        startTime : "",
+        endTime : ""
+    },
+    {
+        id : 1,
+        speakerName : "Nitin Bawankule",
+        designation : "Director Google India",
+        speechTitle : "The Business Case for Creating a Great Workplace",
+        startTime : "",
+        endTime : ""
+    },
+]
