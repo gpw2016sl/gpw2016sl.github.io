0b508626068e1d2ad274eba6274218a99561dd25
Yasiru Kassapa <wik2kassa@gmail.com>
2016-07-07 09:37:17 +0530

    changes

--- data.js
+++ data.js
@@ -2,21 +2,51 @@
  * Created by wik2kassa on 7/7/16.
  */
 
-var GPW = [
+var SpeakerArray = [
     {
-        id : 0,
-        speakerName : "Nitin Bawankule",
-        designation : "Director Google India",
-        speechTitle : "The Business Case for Creating a Great Workplace",
-        startTime : "",
-        endTime : ""
+        id: 0,
+        speakerName: "Nitin Bawankule",
+        designation: "Director Google India",
+        speechTitle: "The Business Case for Creating a Great Workplace",
+        startTime: "",
+        endTime: ""
     },
     {
-        id : 1,
-        speakerName : "Nitin Bawankule",
-        designation : "Director Google India",
-        speechTitle : "The Business Case for Creating a Great Workplace",
-        startTime : "",
-        endTime : ""
+        id: 1,
+        speakerName: "Nitin Bawankule",
+        designation: "Director Google India",
+        speechTitle: "The Business Case for Creating a Great Workplace",
+        startTime: "",
+        endTime: ""
     },
 ]
+
+var SpeechArray = [{
+    "speechTitle": "The Business Case for Creating a Great Workplace by Google",
+    "startTime": "",
+    "endTime": ""
+}, {
+    "speechTitle": "Emerging Practices of Gamification in transforming Employee Experience by KNOLSKAPE",
+    "startTime": "",
+    "endTime": ""
+}, {
+    "speechTitle": "Reinventing HR using disruptive practices By Harvard Business Publishing",
+    "startTime": "",
+    "endTime": ""
+}, {
+    "speechTitle": "Best Workplaces lists - busting the myths By Great Place to Work ®",
+    "startTime": "",
+    "endTime": ""
+}, {
+    "speechTitle": "Innovation in driving productivity By Dialog Axiata",
+    "startTime": "",
+    "endTime": ""
+}, {
+    "speechTitle": "Local Case Study : The Multi National Approach to creating a local Great Work Place By DHL",
+    "startTime": "",
+    "endTime": ""
+}, {
+    "speechTitle": "Thought Leadership : The Great Workplace of the Future By Great Place to Work® India",
+    "startTime": "",
+    "endTime": ""
+}];
